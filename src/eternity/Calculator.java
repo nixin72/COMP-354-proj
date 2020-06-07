16e462e65f1a14e58dce1d66c9c8b35b32dc746b
Delessar <daniel.edery@hotmail.com>
2020-06-07 15:17:51 -0400

    merging from fork
    merging code from my fork (Daniel)

--- src/eternity/Calculator.java
+++ src/eternity/Calculator.java
@@ -1,8 +1,10 @@
 package eternity;
 
 import java.util.ArrayList;
+import java.util.function.Function;
 
 import eternity.Errors.SquareRootOfNegativeException;
+import eternity.Errors.DivideByZeroException;
 
 public class Calculator {
 	// Functions we have to do
@@ -52,9 +54,18 @@ public class Calculator {
 		return x;
 	}
 
+	/**
+	 * Mean Absolute Value "MAD" of a ArrayList of Double
+	 * @param x the ArrayList
+	 * @return the Double Mean Absolute Value
+	 */
 	public static double mean_absolute_deviation(ArrayList<Double> x) {
-		return x.get(0);
+		//find the mean of the array given
+		double meanValue = mean(x);
+	    //find difference between array and the mean.
+		return diffMean(x, meanValue);		
 	}
+	
 
 	public static double standard_deviation(ArrayList<Double> x) {
 		double mean, variance, std_dev;
@@ -68,6 +79,13 @@ public class Calculator {
 ///////////////////////////////////////////////////////////////////////////////
 	// Helper functions
 ///////////////////////////////////////////////////////////////////////////////
+
+	/**
+	* Create an ArrayList of difference between an array of double and the mean
+	* */
+	private static double diffMean(ArrayList<Double> args, double mean){
+		return sum(map(args, x->abs(mean-x))) / args.size();
+	}
 	
 	public static double absolute_value(double x) {
 		if (x < 0) {
@@ -75,6 +93,15 @@ public class Calculator {
 		}
 
 		return x;
+	}
+
+	
+	/**
+	* return the absolute value of a double
+	* */
+	public static double abs(double value) {
+		//If it is negative return the value * -1, else return the number.
+		return (value < 0) ? (-1) * value : value;
 	}
 	
 	public static double square_root(double x) {
@@ -94,22 +121,47 @@ public class Calculator {
 	    return approx;
 	}
 
+	/**
+	 * Sum of a list divided by the number of items
+	 * @param numbers the list of numbers to find a mean.
+	 * @return the mean.
+	 */
 	public static double mean(ArrayList<Double> numbers) {
-		double sum = 0;
-		for (int i = 0; i < numbers.size() ; i++) {
-			sum += numbers.get(i);
-		}
-
-		return sum / numbers.size();
+		//throw divided by zero exception if no numbers are in the list
+		if(numbers.size() == 0) throw new DivideByZeroException();
+		return sum(map(numbers, x->x)) / numbers.size();
 	}
 
 	public static double variance(double mean, ArrayList<Double> numbers) {
-		for (int i = 0; i < numbers.size(); i++) {
-			double diff = numbers.get(i) - mean;
-			numbers.set(i, diff * diff);
+		//variance is (mean - x)^2/size of array
+		return sum(map(numbers, x->(mean-x)*(mean-x))) / numbers.size();
+	}
+	
+	/**
+	 * Apply a function given by fn to all items in the list numbers.
+	 * @param numbers the list to modify by a function fn.
+	 * @param fn a given function lamda.
+	 * @return
+	 */
+	public static ArrayList<Double> map(ArrayList<Double> numbers, Function<Double,Double> fn) {
+		//for each item in the list apply the function given by the lamda expression (e.i: x-> (mean - x))
+		for (var i = 0; i < numbers.size() ; i++) {
+			numbers.set(i, fn.apply(numbers.get(i)));
 		}
-
-		return Calculator.mean(numbers);
+		return numbers;
+	}
+	
+	/**
+	 * Sum all items in a list.
+	 * @param numbers the list of numbers to sum.
+	 * @return sum total
+	 */
+	public static double sum(ArrayList<Double> numbers) {
+		double ret = 0;
+		for (var i = 0; i < numbers.size() ; i++) {
+			ret += numbers.get(i);
+		}	
+		return ret;
 	}
 
 }
