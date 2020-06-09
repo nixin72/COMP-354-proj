f90e72acaecba9701350c0bce7a174c76787a7b1
Philip Dumaresq <phdumaresq@gmail.com>
2020-06-08 21:19:24 -0400

    Added a new exception for raising 0 to a negative exponent

---
+++ src/eternity/Errors/NegativePowerOfZeroException.java
@@ -0,0 +1,25 @@
+package eternity.Errors;
+
+public class NegativePowerOfZeroException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NegativePowerOfZeroException() {}
+
+    public NegativePowerOfZeroException(String message) {
+        super(message);
+    }
+
+    public NegativePowerOfZeroException(Throwable cause) {
+        super(cause);
+    }
+
+    public NegativePowerOfZeroException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NegativePowerOfZeroException(
+            String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
