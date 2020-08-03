f600a4535b33bd9c6d2fd77f0f0769d1943febc3
maryam Eskandari <43692092+meskandari@users.noreply.github.com>
2020-08-03 14:22:39 -0400

    Controller changeist: - fix the typo - change font size -use dictionary as btn texts - implement controller class -implement parser class

--- src/eternity/Calculator.java
+++ src/eternity/Calculator.java
@@ -5,378 +5,310 @@ import Errors.SquareRootOfNegativeException;
 import Errors.DivideByZeroException;
 
 public class Calculator {
-  private static double PI = 0;
-  private static double E = 0;
+    private static double PI = 0;
+    private static double E = 0;
 
-  public static double PI() {
-    if (PI != 0) return PI;
+    public static double PI() {
+        if (PI != 0) return PI;
 
-    double Pi = 0.0;
+        double Pi = 0.0;
 
-    for (int i = 1; i < 100000; i++) {
-      Pi += (mod(i, 2) == 0) ? -1 / (2 * (double) i - 1) : 1 / (2 * (double) i - 1);
-    }
-    PI = Pi * 4;
-    return PI;
-  }
-
-  public static double E() {
-    if (E != 0) return E;
-
-    double e = 1;
-    for (int i = 1; i < 32; i++) {
-      e = e + 1 / (double) (factorial(i));
-    }
-    E = e;
-
-    return E;
-  }
-
-  public static int mod(int base, int divider) {
-    int maxDividend = base / divider;
-    int result = base - (maxDividend * divider);
-    return result;
-  }
-
-  public static double sin(double x) {
-    double Pi = Calculator.PI();
-
-    double theta = x * (Pi / 180); // calculate for degree
-
-    double sinx = 0;
-    for (int i = 1, power = 1; i <= 10; i++, power += 2) {
-      double term = 0;
-      int powerFactorial = factorial(power);
-      double powerOfx = Calculator.power(theta, power);
-      if (Calculator.mod(i, 2) == 0) {
-        term = -powerOfx / powerFactorial;
-      } else {
-        term = powerOfx / powerFactorial;
-      }
-      sinx += term;
-    }
-    return sinx;
-  }
-
-  public static double ten_to_the_x(double x) {
-    final double ln10 = log_e_x(10);
-
-    if (x == 0) {
-      return 1;
-    }
-    if (x == 1) {
-      return 10;
-    }
-    if (x == -1) {
-      return 0.1;
+        for (int i = 1; i < 100000; i++) {
+            Pi += (mod(i, 2) == 0) ? -1 / (2 * (double) i - 1) : 1 / (2 * (double) i - 1);
+        }
+        PI = Pi * 4;
+        return PI;
     }
 
-    double new_e_exponent = x * ln10;
-    double sum = 1;
+    public static double E() {
+        if (E != 0) return E;
 
-    for (int i = 1000; i > 0; --i) {
-      sum = 1 + new_e_exponent * sum / (i);
+        double e = 1;
+        for (int i = 1; i < 32; i++) {
+            e = e + 1 / (double) (factorial(i));
+        }
+        E = e;
+
+        return E;
     }
 
-    if ((x == (int) x) && (x > 0)) {
-      sum = (int) (sum * 100 + 0.5) / 100;
+    public static int mod(int base, int divider) {
+        int maxDividend = base / divider;
+        int result = base - (maxDividend * divider);
+        return result;
     }
 
-    return sum;
-  }
+    public static double sin(double x) {
+        double Pi = Calculator.PI();
 
-  public static double x_to_the_y(double x, double y) {
-    if (x == 0) {
-      if (y > 0) {
-        return (0);
-      } else {
-        return Double.NEGATIVE_INFINITY;
-      }
-    }
-    if (x == 1) {
-      return (1);
+        double theta = x * (Pi / 180); // calculate for degree
+
+        double sinx = 0;
+        for (int i = 1, power = 1; i <= 10; i++, power += 2) {
+            double term = 0;
+            int powerFactorial = (int) factorial(power);
+            double powerOfx = Calculator.power(theta, power);
+            if (Calculator.mod(i, 2) == 0) {
+                term = -powerOfx / powerFactorial;
+            } else {
+                term = powerOfx / powerFactorial;
+            }
+            sinx += term;
+        }
+        return sinx;
     }
 
-    if (x == -1) {
-      if (y != (int) y) {
-        return (-1);
-      } else {
-        if (Calculator.CheckEvenOdd((int) y)) {
-          return (1);
+    public static double ten_to_the_x(double x) {
+        final double ln10 = log_e_x(10);
+
+        if (x == 0) {
+            return 1;
+        }
+        if (x == 1) {
+            return 10;
+        }
+        if (x == -1) {
+            return 0.1;
+        }
+
+        double new_e_exponent = x * ln10;
+        double sum = 1;
+
+        for (int i = 1000; i > 0; --i) {
+            sum = 1 + new_e_exponent * sum / (i);
+        }
+
+        if ((x == (int) x) && (x > 0)) {
+            sum = (int) (sum * 100 + 0.5) / 100;
+        }
+
+        return sum;
+    }
+
+    public static boolean CheckEvenOdd(int number) {
+        double result = number / 2;
+        // true = even, false = odd
+        return result == (int) result;
+    }
+
+    public static double newton_sqrt(double initialGuess, double base) {
+        double threshold = 0.00001;
+        double guess = initialGuess;
+        double newGuess = (guess + base / guess) / 2;
+        while (absolute_value(guess - newGuess) > threshold) {
+            guess = newGuess;
+            newGuess = (guess + base / guess) / 2;
+        }
+        return newGuess;
+    }
+
+    public static double power(double base, double exponent) {
+        boolean basePositive = base > 0 ? true : false;
+        boolean exponentPositive = exponent > 0 ? true : false;
+        if (!basePositive) {
+            if (Calculator.mod((int) exponent, 2) == 0) {
+                basePositive = true;
+                base *= -1;
+            } else if (!exponentPositive) {
+                throw new ArithmeticException("Operation not allowed");
+            } else {
+                base *= -1;
+            }
+        }
+
+        if (!exponentPositive) {
+            exponent *= -1;
+        }
+
+        double threshold = 0.00001;
+        // if exponent is an integer
+        if (exponent == (int) exponent) {
+            if (exponent < 0) {
+                exponent *= -1;
+                double result = base;
+                while (exponent-- > 1) {
+                    result *= base;
+                }
+                return (basePositive ? 1 : -1) * (1 / result);
+            } else {
+                double result = base;
+                while (exponent-- > 1) {
+                    result *= base;
+                }
+                return (basePositive ? 1 : -1) * result;
+            }
+        }
+
+        // if exponent is a floating point number
+        if (exponent >= 1) {
+            double temp = Calculator.power(base, exponent / 2);
+            return (basePositive ? 1 : -1) * (temp * temp);
         } else {
-          return (-1);
+            double low = 0;
+            double high = 1.0;
+            double sqr = Calculator.newton_sqrt(base / 2, base);
+            double acc = sqr;
+            double middle = high / 2;
+
+            while (absolute_value(middle - exponent) > threshold) {
+                sqr = Calculator.newton_sqrt(sqr / 2, sqr);
+                if (middle <= exponent) {
+                    low = middle;
+                    acc *= sqr;
+                } else {
+                    high = middle;
+                    acc *= (1 / sqr);
+                }
+                middle = (low + high) / 2;
+            }
+            return (exponentPositive
+                    ? (basePositive ? 1 : -1) * acc
+                    : 1 / ((basePositive ? 1 : -1) * acc));
         }
-      }
     }
 
-    if (y == 0) {
-      return (1);
+    /**
+     * Natural log Author: Isaac Doré - 40043159
+     *
+     * <p>Leveraging Taylor series of the exp() function. The function f(y)= exp(y) - x will be zero
+     * when ln(x)=y. We can use this with to converge towards the solution. Computed using Newton's
+     * method : x_{n+1} = x_n - f(x_n)/f'(x_{n}) Newton's method gives us good convergence, iterating
+     * 10 times gives us answer accurate to the 10^(-16) for values [x, x+10]. Note: Less precise as x
+     * moves away from 1. Note: Computing 100 iteration for maximum precision over a larger domain,
+     * but more expensive.
+     *
+     * @param x
+     * @return ln(x)
+     */
+    public static double log_e_x(double x) {
+        if (x <= 0) return Double.NEGATIVE_INFINITY; // Avoid floating point error around x=0.
+
+        double res = x - 1; // Initial guess for Newton's Method
+
+        for (int i = 0; i < 100; i++) res = res - 1 + x * Euler.exp(-res);
+
+        return res;
     }
 
-    if (y == 1) {
-      return (x);
+    public static double log_10_x(double x) {
+        return x;
     }
 
-    if (y < 1 && (y == (int) y)) {
-      double result = x;
-      while (y-- > 1) {
-        result *= x;
-      }
+    /**
+     * Mean Absolute Value "MAD" of a ArrayList of Double
+     *
+     * @param x the ArrayList
+     * @return the Double Mean Absolute Value
+     */
+    public static double mean_absolute_deviation(ArrayList<Double> x) {
+        // find the mean of the array given
+        double meanValue = mean(x);
+
+        // find difference between array and the mean.
+        return diffMean(x, meanValue);
     }
 
-    if (y == (int) y) {
-      if (y < 0) {
-        y *= -1;
-        double result = x;
-        while (y-- > 1) {
-          result *= x;
+    public static double standard_deviation(ArrayList<Double> x) {
+        double mean = Calculator.mean(x);
+        double variance = Calculator.variance(mean, x);
+        double std_dev = Calculator.square_root(variance);
+
+        return std_dev;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // Helper functions
+    ///////////////////////////////////////////////////////////////////////////////
+
+    public static double absolute_value(double x) {
+        if (x < 0) {
+            x *= -1;
         }
-        result = 1 / result;
-        return (result);
-      } else {
-        double result = x;
-        while (y-- > 1) {
-          result *= x;
+
+        return x;
+    }
+
+    public static double square_root(double x) {
+        if (x < 0) {
+            throw new SquareRootOfNegativeException();
         }
-        return (result);
-      }
-    }
 
-    // if we reach this point y is not an int, no need to test for it.
-    // find the factor of 10 that will turn y into an integer
-
-    if (y > 1) {
-      // TODO decimal power
-
-    } else {
-      // if we reach this point we know that y is negative
-      // TODO decimal power
-    }
-    return x;
-  }
-
-  public static boolean CheckEvenOdd(int number) {
-    double result = number / 2;
-    // true = even, false = odd
-    return result == (int) result;
-  }
-
-  public static double newton_sqrt(double initialGuess, double base) {
-    double threshold = 0.00001;
-    double guess = initialGuess;
-    double newGuess = (guess + base / guess) / 2;
-    while (absolute_value(guess - newGuess) > threshold) {
-      guess = newGuess;
-      newGuess = (guess + base / guess) / 2;
-    }
-    return newGuess;
-  }
-
-  public static double power(double base, double exponent) {
-    boolean basePositive = base > 0 ? true : false;
-    boolean exponentPositive = exponent > 0 ? true : false;
-    if (!basePositive) {
-      if (Calculator.mod((int) exponent, 2) == 0) {
-        basePositive = true;
-        base *= -1;
-      } else if (!exponentPositive) {
-        throw new ArithmeticException("Operation not allowed");
-      } else {
-        base *= -1;
-      }
-    }
-
-    if (!exponentPositive) {
-      exponent *= -1;
-    }
-
-    double threshold = 0.00001;
-    // if exponent is an integer
-    if (exponent == (int) exponent) {
-      if (exponent < 0) {
-        exponent *= -1;
-        double result = base;
-        while (exponent-- > 1) {
-          result *= base;
+        double approx = 0;
+        for (double i = x / 2; x >= 0; i = approx) {
+            approx = i - (i * i - x) / (i * 2);
+            if (Calculator.absolute_value(i - approx) < 0.0001) {
+                break;
+            }
+            i = approx;
         }
-        return (basePositive ? 1 : -1) * (1 / result);
-      } else {
-        double result = base;
-        while (exponent-- > 1) {
-          result *= base;
+
+        return approx;
+    }
+
+    /**
+     * Sum of a list divided by the number of items
+     *
+     * @param numbers the list of numbers to find a mean.
+     * @return the mean.
+     */
+    public static double mean(ArrayList<Double> numbers) {
+        // throw divided by zero exception if no numbers are in the list
+        if (numbers.size() == 0) {
+            throw new DivideByZeroException();
         }
-        return (basePositive ? 1 : -1) * result;
-      }
+
+        return sum(map(numbers, x -> x)) / numbers.size();
     }
 
-    // if exponent is a floating point number
-    if (exponent >= 1) {
-      double temp = Calculator.power(base, exponent / 2);
-      return (basePositive ? 1 : -1) * (temp * temp);
-    } else {
-      double low = 0;
-      double high = 1.0;
-      double sqr = Calculator.newton_sqrt(base / 2, base);
-      double acc = sqr;
-      double middle = high / 2;
+    public static double variance(double mean, ArrayList<Double> numbers) {
+        // variance is (mean - x)^2/size of array
+        return sum(map(numbers, x -> (mean - x) * (mean - x))) / numbers.size();
+    }
 
-      while (absolute_value(middle - exponent) > threshold) {
-        sqr = Calculator.newton_sqrt(sqr / 2, sqr);
-        if (middle <= exponent) {
-          low = middle;
-          acc *= sqr;
-        } else {
-          high = middle;
-          acc *= (1 / sqr);
+    /**
+     * Create an ArrayList of difference between an array of double and the mean
+     */
+    private static double diffMean(ArrayList<Double> args, double mean) {
+        return sum(map(args, x -> absolute_value(mean - x))) / args.size();
+    }
+
+    /**
+     * Apply a function given by fn to all items in the list numbers.
+     *
+     * @param numbers the list to modify by a function fn.
+     * @param fn      a given function lamda.
+     * @return
+     */
+    public static ArrayList<Double> map(ArrayList<Double> numbers, Function<Double, Double> fn) {
+        // for each item in the list apply the function given by the lamda expression (e.i: x-> (mean -
+        // x))
+        for (var i = 0; i < numbers.size(); i++) {
+            numbers.set(i, fn.apply(numbers.get(i)));
         }
-        middle = (low + high) / 2;
-      }
-      return (exponentPositive
-          ? (basePositive ? 1 : -1) * acc
-          : 1 / ((basePositive ? 1 : -1) * acc));
-    }
-  }
 
-  /**
-   * Natural log Author: Isaac Doré - 40043159
-   *
-   * <p>Leveraging Taylor series of the exp() function. The function f(y)= exp(y) - x will be zero
-   * when ln(x)=y. We can use this with to converge towards the solution. Computed using Newton's
-   * method : x_{n+1} = x_n - f(x_n)/f'(x_{n}) Newton's method gives us good convergence, iterating
-   * 10 times gives us answer accurate to the 10^(-16) for values [x, x+10]. Note: Less precise as x
-   * moves away from 1. Note: Computing 100 iteration for maximum precision over a larger domain,
-   * but more expensive.
-   *
-   * @param x
-   * @return ln(x)
-   */
-  public static double log_e_x(double x) {
-    if (x <= 0) return Double.NEGATIVE_INFINITY; // Avoid floating point error around x=0.
-
-    double res = x - 1; // Initial guess for Newton's Method
-
-    for (int i = 0; i < 100; i++) res = res - 1 + x * Euler.exp(-res);
-
-    return res;
-  }
-
-  public static double log_10_x(double x) {
-    return x;
-  }
-
-  /**
-   * Mean Absolute Value "MAD" of a ArrayList of Double
-   *
-   * @param x the ArrayList
-   * @return the Double Mean Absolute Value
-   */
-  public static double mean_absolute_deviation(ArrayList<Double> x) {
-    // find the mean of the array given
-    double meanValue = mean(x);
-
-    // find difference between array and the mean.
-    return diffMean(x, meanValue);
-  }
-
-  public static double standard_deviation(ArrayList<Double> x) {
-    double mean = Calculator.mean(x);
-    double variance = Calculator.variance(mean, x);
-    double std_dev = Calculator.square_root(variance);
-
-    return std_dev;
-  }
-
-  ///////////////////////////////////////////////////////////////////////////////
-  // Helper functions
-  ///////////////////////////////////////////////////////////////////////////////
-
-  public static double absolute_value(double x) {
-    if (x < 0) {
-      x *= -1;
+        return numbers;
     }
 
-    return x;
-  }
+    /**
+     * Sum all items in a list.
+     *
+     * @param numbers the list of numbers to sum.
+     * @return sum total
+     */
+    public static double sum(ArrayList<Double> numbers) {
+        double ret = 0;
+        for (var i = 0; i < numbers.size(); i++) {
+            ret += numbers.get(i);
+        }
 
-  public static double square_root(double x) {
-    if (x < 0) {
-      throw new SquareRootOfNegativeException();
+        return ret;
     }
 
-    double approx = 0;
-    for (double i = x / 2; x >= 0; i = approx) {
-      approx = i - (i * i - x) / (i * 2);
-      if (Calculator.absolute_value(i - approx) < 0.0001) {
-        break;
-      }
-      i = approx;
+    public static double factorial(int x) {
+        if (x == 0) {
+            return 1;
+        }
+
+        return x * factorial(x - 1);
     }
-
-    return approx;
-  }
-
-  /**
-   * Sum of a list divided by the number of items
-   *
-   * @param numbers the list of numbers to find a mean.
-   * @return the mean.
-   */
-  public static double mean(ArrayList<Double> numbers) {
-    // throw divided by zero exception if no numbers are in the list
-    if (numbers.size() == 0) {
-      throw new DivideByZeroException();
-    }
-
-    return sum(map(numbers, x -> x)) / numbers.size();
-  }
-
-  public static double variance(double mean, ArrayList<Double> numbers) {
-    // variance is (mean - x)^2/size of array
-    return sum(map(numbers, x -> (mean - x) * (mean - x))) / numbers.size();
-  }
-
-  /** Create an ArrayList of difference between an array of double and the mean */
-  private static double diffMean(ArrayList<Double> args, double mean) {
-    return sum(map(args, x -> absolute_value(mean - x))) / args.size();
-  }
-
-  /**
-   * Apply a function given by fn to all items in the list numbers.
-   *
-   * @param numbers the list to modify by a function fn.
-   * @param fn a given function lamda.
-   * @return
-   */
-  public static ArrayList<Double> map(ArrayList<Double> numbers, Function<Double, Double> fn) {
-    // for each item in the list apply the function given by the lamda expression (e.i: x-> (mean -
-    // x))
-    for (var i = 0; i < numbers.size(); i++) {
-      numbers.set(i, fn.apply(numbers.get(i)));
-    }
-
-    return numbers;
-  }
-
-  /**
-   * Sum all items in a list.
-   *
-   * @param numbers the list of numbers to sum.
-   * @return sum total
-   */
-  public static double sum(ArrayList<Double> numbers) {
-    double ret = 0;
-    for (var i = 0; i < numbers.size(); i++) {
-      ret += numbers.get(i);
-    }
-
-    return ret;
-  }
-
-  public static int factorial(int x) {
-    if (x == 0) {
-      return 1;
-    }
-
-    return x * factorial(x - 1);
-  }
 }
---
+++ src/eternity/MathExpressionParser.java
@@ -0,0 +1,228 @@
+import java.util.*;
+
+public class MathExpressionParser {
+    // Associativity constants for operators
+    private static final int LEFT_ASSOC = 0;
+    private static final int RIGHT_ASSOC = 1;
+    private static final int UNARYOPERATOR = 0;
+    private static final int BINARYOPERATOR = 1;
+
+    // Operators
+    private static final Map<String, int[]> OPERATORS = new HashMap<String, int[]>();
+
+    static {
+        // Map<"token", []{precendence, associativity , operatorType}>
+        OPERATORS.put("+", new int[]{1, LEFT_ASSOC, BINARYOPERATOR});
+        OPERATORS.put("-", new int[]{1, LEFT_ASSOC, BINARYOPERATOR});
+        OPERATORS.put("*", new int[]{2, LEFT_ASSOC, BINARYOPERATOR});
+        OPERATORS.put("÷", new int[]{2, LEFT_ASSOC, BINARYOPERATOR});
+
+        OPERATORS.put("Sin", new int[]{1, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("ln", new int[]{1, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("e^", new int[]{3, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("10^", new int[]{3, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("^", new int[]{3, LEFT_ASSOC, BINARYOPERATOR});
+        OPERATORS.put("MAD", new int[]{1, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("Sqrt", new int[]{3, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("Abs", new int[]{1, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("!", new int[]{1, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("μ", new int[]{1, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put("σ", new int[]{1, RIGHT_ASSOC, UNARYOPERATOR});
+        OPERATORS.put(",", new int[]{1, LEFT_ASSOC, BINARYOPERATOR});
+        OPERATORS.put("sign", new int[]{3, LEFT_ASSOC, UNARYOPERATOR});
+    }
+
+    // Test if token is an operator
+    private static boolean isOperator(String token) {
+        return OPERATORS.containsKey(token);
+    }
+
+    // Test associativity of operator token
+    private static boolean isAssociative(String token, int type) {
+        if (!isOperator(token)) {
+            throw new IllegalArgumentException("Invalid token: " + token);
+        }
+
+        if (OPERATORS.get(token)[1] == type) {
+            return true;
+        }
+        return false;
+    }
+
+    // Compare precedence of operators.
+    private static final int cmpPrecedence(String token1, String token2) {
+        if (!isOperator(token1) || !isOperator(token2)) {
+            throw new IllegalArgumentException("Invalid tokens: " + token1
+                    + " " + token2);
+        }
+        int result = OPERATORS.get(token1)[0] - OPERATORS.get(token2)[0];
+        return OPERATORS.get(token1)[0] - OPERATORS.get(token2)[0];
+    }
+
+    // Convert infix expression format into reverse Polish notation
+    public static String[] infixToRPN(ArrayList<String> inputTokens) {
+        ArrayList<String> out = new ArrayList<String>();
+        Stack<String> stack = new Stack<String>();
+
+        // For each token
+        for (String token : inputTokens) {
+            // If token is an operator
+            if (isOperator(token)) {
+                // While stack not empty AND stack top element
+                // is an operator
+                while (!stack.empty() && isOperator(stack.peek())) {
+                    if ((isAssociative(token, LEFT_ASSOC) &&
+                            cmpPrecedence(token, stack.peek()) <= 0) ||
+                            (isAssociative(token, RIGHT_ASSOC) &&
+                                    cmpPrecedence(token, stack.peek()) < 0)) {
+                        out.add(stack.pop());
+                        continue;
+                    }
+                    break;
+                }
+                // Push the new operator on the stack
+                stack.push(token);
+            }
+            // If token is a left bracket '('
+            else if (token.equals("(")) {
+                stack.push(token);  //
+            }
+            // If token is a right bracket ')'
+            else if (token.equals(")")) {
+                while (!stack.empty() && !stack.peek().equals("(")) {
+                    out.add(stack.pop());
+                }
+                stack.pop();
+            }
+            // If token is a number
+            else {
+                if (token.equals("π")) {
+                    out.add(String.valueOf(Calculator.PI()));
+                } else {
+                    out.add(token);
+                }
+            }
+        }
+        while (!stack.empty()) {
+            out.add(stack.pop());
+        }
+        String[] output = new String[out.size()];
+        return out.toArray(output);
+    }
+
+    public static double RPNtoDouble(String[] tokens) {
+        Stack<ArrayList<Double>> Ostack = new Stack<ArrayList<Double>>();
+        ArrayList<Double> resultArr = new ArrayList(2);
+
+        // For each token
+        for (String token : tokens) {
+            if (token.equals("")) {
+                continue;
+            }
+            // If the token is a value push it onto the stack
+            if (!isOperator(token)) {
+                ArrayList<Double> temp = new ArrayList<Double>(10);
+                temp.add(Double.valueOf(token));
+                Ostack.push(temp);
+            } else {
+
+                if (OPERATORS.containsKey(token)) {
+                    if (OPERATORS.get(token)[2] == 0) {
+                        ArrayList<Double> d = Ostack.pop();
+                        resultArr = performUnaryOperation(token, d);
+                    } else {
+                        ArrayList<Double> d2 = Ostack.pop();
+                        ArrayList<Double> d1 = Ostack.pop();
+                        resultArr = performBinaryOperation(token, d1, d2);
+                    }
+                }
+                // Push result onto stack
+                Ostack.push(resultArr);
+            }
+        }
+
+        return Double.valueOf(resultArr.get(0));
+    }
+
+    public static ArrayList<Double> performUnaryOperation(String operand, ArrayList<Double> a) {
+        ArrayList<Double> result = new ArrayList<>(10);
+        switch (operand) {
+            case "Sin":
+                result.add(Calculator.sin(a.get(0)));
+                break;
+            case "MAD":
+                result.add(Calculator.mean_absolute_deviation(a));
+                break;
+            case "!":
+                Integer integer = (int) ((double) a.get(0));
+                result.add(Calculator.factorial(integer));
+                break;
+            case "ln":
+                result.add(Calculator.log_e_x(a.get(0)));
+                break;
+            case "Sqrt":
+                result.add(Calculator.square_root(a.get(0)));
+                break;
+            case "Abs":
+                result.add(Calculator.absolute_value(a.get(0)));
+                break;
+            case "μ":
+                result.add(Calculator.mean(a));
+                break;
+            case "σ":
+                result.add(Calculator.standard_deviation(a));
+                break;
+            case "e^":
+                result.add(Euler.exp(a.get(0)));
+                break;
+            case "10^":
+                result.add(Calculator.ten_to_the_x(a.get(0)));
+                break;
+        }
+        return result;
+    }
+
+    public static ArrayList<Double> performBinaryOperation(String operand, ArrayList<Double> a1, ArrayList<Double> a2) {
+        ArrayList<Double> result = new ArrayList<>(10);
+        switch (operand) {
+            case "+":
+                result.add(a1.get(0) + a2.get(0));
+                break;
+            case "-":
+                result.add(a1.get(0) - a2.get(0));
+                break;
+            case "*":
+                result.add(a1.get(0) * a2.get(0));
+                break;
+            case "÷":
+                result.add(a1.get(0) / a2.get(0));
+                break;
+            case "^":
+                result.add(Calculator.power(a1.get(0), a2.get(0)));
+                break;
+            case ",":
+                a1.add(a2.remove(0));
+                result = a1;
+                break;
+        }
+        return result;
+    }
+
+//    public static void main(String[] args) {
+//        ArrayList<String> input = new ArrayList<>(4);
+//        input.add("4");
+//        input.add("+");
+//        input.add("2");
+//        input.add("*");
+//        input.add("3");
+//        String[] output = infixToRPN(input);
+//
+//        // Build output RPN string minus the commas
+//        for (String token : output) {
+//            System.out.print(token + " ");
+//        }
+//
+//        // Feed the RPN string to RPNtoDouble to give result
+//        Double result = RPNtoDouble(output);
+//    }
+}
